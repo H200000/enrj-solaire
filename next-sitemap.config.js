350d3edede95772b99f916bc759bf299c6a8eac9
Arsène Leguen de Kergolan <arsene@ntechparis.com>
2026-04-01 08:47:39 +0200

    fix: align siteUrl to enr-j.com (sans www) pour corriger canonicals et sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.enr-j.com",
+  siteUrl: "https://enr-j.com",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "weekly",
